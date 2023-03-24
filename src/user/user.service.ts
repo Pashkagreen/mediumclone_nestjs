d18bc9720a9712177e52b655e7c72ec47d7eb7c2
Paul Grin <pashkagrin@gmail.com>
2023-03-24 12:55:56 +0300

    user registration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import {Repository} from 'typeorm';
 import {InjectRepository} from '@nestjs/typeorm';
+import {sign} from 'jsonwebtoken'
 
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './user.entity';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -14,5 +16,18 @@ export class UserService {
     Object.assign(newUser, createUserDto)
     
     return await this.userRepository.save(newUser)
+  }
+
+  generateJWT(user: UserEntity): string {
+    return sign({id: user.id, username: user.username, email: user.email}, 'SECRET_KEY')
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJWT(user)
+      }
+    }
   }
 }
